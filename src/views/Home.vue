8ad9570e610312f786cb8e846e692ed1df93a925
rzx <770304867@qq.com>
2019-05-20 14:21:32 +0800

    细节修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+      <el-tab-pane label="发电厂" name="first">发电厂</el-tab-pane>
+      <el-tab-pane label="发电机" name="second">机组</el-tab-pane>
+      <el-tab-pane label="交流线端" name="third">线端</el-tab-pane>
+      <el-tab-pane label="变压器绕组" name="fourth">绕组</el-tab-pane>
+      <el-tab-pane label="并联电容器" name="fifth">并联电容器</el-tab-pane>
+      <el-tab-pane label="并联电抗器" name="six">并联电抗器</el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  data() {
+    return {
+      activeName: "first"
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
   }
-}
+};
 </script>
